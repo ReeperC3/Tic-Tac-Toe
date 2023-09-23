a6078ceb565e15e18e4f50420e45bfdf02d5d4d5
ReeperC3 <cian3neiland@gmail.com>
2023-09-23 16:53:20 +0100

    Made it so that clicking the squares puts in x's and o's

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,38 @@
 const gameBoard = (() => {
-  const board = [];
-  return { board };
+
+  const playerFactory = (name, mark, turn) => {
+    return { name, mark, turn }
+  }
+
+  let player1 = playerFactory('P1', 'x', true)
+  let player2 = playerFactory('P2', 'o', false)
+
+  let turn = 0
+
+  const playerTurn = () => {
+    const box = document.querySelectorAll('.box');
+    box.forEach( box => {
+      box.addEventListener('click', e => {
+        if (turn === 0) {
+          box.textContent = 'x'
+          turn = 1
+        } else if (turn === 1) {
+          box.textContent = 'o'
+          turn = 0
+        }
+      })  
+    })
+  }
+  return { playerTurn }
 })();
+gameBoard.playerTurn()
 
-const player = () => {
 
-}
+
+
+
+
+
+
+            
 
